2702e97bc847a71563194d84c2092bf69c800b23
AumPanudach <6406021611026@fitm.kmutnb.ac.th>
2023-01-19 11:23:41 +0700

    update

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const experss = require("express");
+const app = experss();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
