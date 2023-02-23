903d9ac115bd14793bff247f692c6be16f810bf9
AumPanudach <6406021611026@fitm.kmutnb.ac.th>
2023-02-23 10:40:54 +0700

    update

--- src/CRUDBookSQLite.js
+++ src/CRUDBookSQLite.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3');
 const app = express();
 const db = new sqlite3.Database('./Database./Book.sqlite');
 app.use(express.json());
-db.run(`CREATE TABLE IF NOT EXISTS books ( id INTEGER PRIMARY KEY,title TEXT author TEXT)`);
+db.run(`CREATE TABLE IF NOT EXISTS books ( id INTEGER PRIMARY KEY,title TEXT ,author TEXT)`);
 app.get('/books',(req,res) => {
     db.all('SELECT * FROM books',(err,rows) => {
         if(err){
@@ -42,7 +42,7 @@ app.post('/books',(req,res) => {
 
 app.put('/books/:id', (req,res) => {
     const book = req.body;
-    db.run('UPDATE books SET title = ? , author = ? WHERE id = ?', book.title , book.author , req.params.id , function(err) {
+    db.run('UPDATE books SET title = ? , author = ? WHERE id = ?', book.title , book.author , req.params.id, function(err) {
         if(err){
             res.status(500).send(err);
         }else{
